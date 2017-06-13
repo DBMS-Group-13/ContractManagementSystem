0afe64237a09ac98e6c6296cc8cfe1d211c42451
Dashao11 <pengyusenkai@126.com>
2017-06-13 08:51:33 +0800

    change Dao

---
+++ src/web/ToDraftServlet.java
@@ -0,0 +1,48 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet for accessing draft contract page
+ */
+public class ToDraftServlet extends HttpServlet {
+
+	/**
+	 * Jump to draft contract page
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//Set the request's character encoding
+		request.setCharacterEncoding("UTF-8");
+		
+		// Declare session
+		HttpSession session = null;
+		// Get session by using request object
+		session = request.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		// If user is not login, jump to login page
+		if (userId == null) {
+			response.sendRedirect("toLogin");
+		}else {
+			// Forward to draft page			
+			request.getRequestDispatcher("/addContract.jsp").forward(request, response);
+		}
+	}
+	
+	/**
+	 * Process GET requests
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// call doPost() to process request
+		this.doPost(request, response);
+	}
+
+}
